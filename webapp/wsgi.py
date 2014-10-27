75ed1a76f2a606bd89c77062a669764cfdaca2d8
Joaquin Sargiotto <joaquinsargiotto@gmail.com>
2014-10-27 09:35:25 -0300

    Adapting for apache2

--- webapp/wsgi.py
+++ webapp/wsgi.py
@@ -8,6 +8,12 @@ https://docs.djangoproject.com/en/1.7/howto/deployment/wsgi/
 """
 
 import os
+import sys
+
+WEBAPPPATH = os.path.dirname(__file__)
+PROJECTPATH = os.path.dirname(WEBAPPPATH)
+sys.path.append(PROJECTPATH)
+
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "webapp.settings")
 
 from django.core.wsgi import get_wsgi_application
